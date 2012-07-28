497f2b842dc387f64c7eaf4aada73a49b5d156da
terrycojones <terry@fluidinfo.com>
2012-07-27 21:10:13 -0400

    Update DOM id for sidebar close image.

--- iframe.js
+++ iframe.js
@@ -4,7 +4,7 @@
  *  It should only do things if it's in the sidebar frame, not
  * in the main tab code.
  */
-var close = document.getElementById('fluidinfo-sidebar-close');
+var close = document.getElementById('loveme.do-sidebar-close');
 
 if (close) {
     // Listen for requests from the background page.
